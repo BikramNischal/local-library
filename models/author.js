f0dbc1d60a383756c0a4065ba8aaa7392d19afb1
PsychicGeek <bikram.bh10@gmail.com>
2024-01-18 18:36:33 +0545

    Added EJS Templates

--- models/author.js
+++ models/author.js
@@ -12,7 +12,7 @@ const AuthorSchema = new Schema({
 AuthorSchema.virtual("name").get(function (){
     let fullname = "";
     if(this.first_name && this.last_name)
-        fullname = `$(this.first_name) $(this.last_name)`;
+        fullname = this.first_name + " " + this.last_name;
 
     return fullname;
 });
