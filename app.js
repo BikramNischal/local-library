cf740e2f6df4d34aa3c9379d2a2ed2ab443eca71
PsychicGeek <bikram.bh10@gmail.com>
2024-01-31 08:56:38 +0545

    changes for production

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require("compression");
+const helmet = require("helmet");
+const debug = require("debug")("Database:connection");
 
 
 // Set up mongoose connection
@@ -11,19 +14,33 @@ const mongoose  = require("mongoose");
 mongoose.set("strictQuery", false);
 const libraryDB = process.env.DB_URL;
  
-testConnection().catch(err => console.log(err));
+testConnection().catch(err => console.err(err));
 async function testConnection(){
   await mongoose.connect(libraryDB);
-  console.log("Connection Successful!");
+  debug("Database Connection Successful!");
 }
 
 
 // router setup
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
 
 const app = express();
+
+const RateLimit= require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1*60*1000,
+  max: 20,
+});
+
+app.use(limiter);
+
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,10 +50,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
